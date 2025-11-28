ec4a2542bc5c8c269f3e1065887acc901720356a
Vinícius Gonçalves <161911326+ViniciusAndreGoncalves@users.noreply.github.com>
2025-11-28 09:16:10 -0300

    feat: Adicionando init.sql
    Automatizar a criação do banco de dados

---
+++ init.sql
@@ -0,0 +1,27 @@
+-- init.sql
+DROP TABLE IF EXISTS pedidos;
+DROP TABLE IF EXISTS produtos;
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Tabela de Produtos (O recurso escasso)
+CREATE TABLE produtos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    estoque INT CHECK (estoque >= 0), -- A trava nativa (opcional, se quiser ver o erro explodir)
+    versao INT DEFAULT 1 -- Útil para Optimistic Locking
+);
+
+-- Tabela de Pedidos (O histórico)
+CREATE TABLE pedidos (
+    id SERIAL PRIMARY KEY,
+    id_produto INT REFERENCES produtos(id),
+    cliente_id INT,
+    chave_idempotencia UUID UNIQUE, -- Garante que não haja pedidos duplicados
+    quantidade INT,
+    status VARCHAR(20) DEFAULT 'PENDENTE',
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Inserindo um produto com APENAS 5 unidades
+INSERT INTO produtos (id, nome, estoque) VALUES (1, 'Samsung Galaxy A56 256 Gb', 10);
